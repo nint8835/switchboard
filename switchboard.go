2cad52a7b7b1ba4df1a5bf915c1e51c9881bf05a
Riley Flynn <riley@rileyflynn.me>
2022-10-15 17:45:01 -0230

    Remove all command-related code
    Redesigining all of it, no point in keeping it around. Handler code is still fine though, as an entry point

--- switchboard.go
+++ switchboard.go
@@ -2,43 +2,12 @@ package switchboard
 
 import "github.com/bwmarrin/discordgo"
 
-type CommandHandler func(*discordgo.Session, *discordgo.InteractionCreate) error
-
-// Command is a registered Switchboard command.
-type Command struct {
-	*discordgo.ApplicationCommand
-	Handler CommandHandler
-}
-
 // Switchboard is the main type responsible for managing all interactions.
 type Switchboard struct {
-	Commands map[string]Command
-}
-
-// Register a command with Switchboard. Note that this does not register the command with Discord.
-func (switchboard *Switchboard) AddCommand(command *discordgo.ApplicationCommand, handler CommandHandler) error {
-	if _, exists := switchboard.Commands[command.Name]; exists {
-		return ErrCommandAlreadyExists
-	}
-
-	switchboard.Commands[command.Name] = Command{command, handler}
-
-	return nil
-}
-
-func (switchboard *Switchboard) handleInteractionApplicationCommand(session *discordgo.Session, interaction *discordgo.InteractionCreate) error {
-	command, exists := switchboard.Commands[interaction.ApplicationCommandData().Name]
-	if !exists {
-		return ErrUnknownCommand
-	}
-
-	return command.Handler(session, interaction)
 }
 
 func (switchboard *Switchboard) HandleInteractionCreate(session *discordgo.Session, interaction *discordgo.InteractionCreate) error {
 	switch interaction.Type {
-	case discordgo.InteractionApplicationCommand:
-		return switchboard.handleInteractionApplicationCommand(session, interaction)
 	default:
 		return ErrUnsupportedInteractionType
 	}
@@ -46,7 +15,5 @@ func (switchboard *Switchboard) HandleInteractionCreate(session *discordgo.Sessi
 
 // New constructs a new Switchboard instance.
 func New() *Switchboard {
-	return &Switchboard{
-		Commands: make(map[string]Command),
-	}
+	return &Switchboard{}
 }
--- switchboard_test.go
+++ switchboard_test.go
@@ -7,48 +7,8 @@ import (
 	"github.com/bwmarrin/discordgo"
 )
 
-var testCommand = &discordgo.ApplicationCommand{
-	Name:        "test",
-	Description: "test",
-}
-
-var testCommandHandler = func(session *discordgo.Session, interaction *discordgo.InteractionCreate) error {
-	return nil
-}
-
-func TestSwitchboardAddCommand_WithNewCommand(t *testing.T) {
-	switchboard := &Switchboard{
-		make(map[string]Command),
-	}
-
-	err := switchboard.AddCommand(testCommand, testCommandHandler)
-
-	if err != nil {
-		t.Errorf("Expected no error, got %v", err)
-	}
-
-	if _, exists := switchboard.Commands[testCommand.Name]; !exists {
-		t.Error("Command not added to Switchboard")
-	}
-}
-
-func TestSwitchboardAddCommand_WithExistingCommand(t *testing.T) {
-	switchboard := &Switchboard{
-		make(map[string]Command),
-	}
-
-	switchboard.AddCommand(testCommand, testCommandHandler)
-	err := switchboard.AddCommand(testCommand, testCommandHandler)
-
-	if !errors.Is(err, ErrCommandAlreadyExists) {
-		t.Errorf("Expected error %v, got %v", ErrCommandAlreadyExists, err)
-	}
-}
-
 func TestSwitchboardHandleInteraction_WithUnsupportedInteractionType(t *testing.T) {
-	switchboard := &Switchboard{
-		make(map[string]Command),
-	}
+	switchboard := &Switchboard{}
 
 	err := switchboard.HandleInteractionCreate(nil, &discordgo.InteractionCreate{
 		Interaction: &discordgo.Interaction{
@@ -58,87 +18,5 @@ func TestSwitchboardHandleInteraction_WithUnsupportedInteractionType(t *testing.
 
 	if !errors.Is(err, ErrUnsupportedInteractionType) {
 		t.Errorf("Expected error %v, got %v", ErrUnsupportedInteractionType, err)
-	}
-}
-
-func TestSwitchboardHandleInteraction_WithUnknownCommand(t *testing.T) {
-	switchboard := &Switchboard{
-		make(map[string]Command),
-	}
-
-	err := switchboard.HandleInteractionCreate(nil, &discordgo.InteractionCreate{
-		Interaction: &discordgo.Interaction{
-			Type: discordgo.InteractionApplicationCommand,
-			Data: discordgo.ApplicationCommandInteractionData{
-				Name: "test",
-			},
-		},
-	})
-
-	if !errors.Is(err, ErrUnknownCommand) {
-		t.Errorf("Expected error %v, got %v", ErrUnknownCommand, err)
-	}
-}
-
-func TestSwitchboardHandleInteraction_WithKnownCommand(t *testing.T) {
-	switchboard := &Switchboard{
-		make(map[string]Command),
-	}
-
-	called := false
-
-	switchboard.AddCommand(testCommand, func(s *discordgo.Session, ic *discordgo.InteractionCreate) error {
-		called = true
-		return nil
-	})
-
-	err := switchboard.HandleInteractionCreate(nil, &discordgo.InteractionCreate{
-		Interaction: &discordgo.Interaction{
-			Type: discordgo.InteractionApplicationCommand,
-			Data: discordgo.ApplicationCommandInteractionData{
-				Name: "test",
-			},
-		},
-	})
-
-	if err != nil {
-		t.Errorf("Expected no error, got %v", err)
-	}
-
-	if !called {
-		t.Error("Command handler not called")
-	}
-}
-
-func TestSwitchboardHandlerInteraction_WithCommandErrorPropagates(t *testing.T) {
-	switchboard := &Switchboard{
-		make(map[string]Command),
-	}
-
-	expectedError := errors.New("test")
-
-	switchboard.AddCommand(testCommand, func(s *discordgo.Session, ic *discordgo.InteractionCreate) error {
-		return expectedError
-	})
-
-	err := switchboard.HandleInteractionCreate(nil, &discordgo.InteractionCreate{
-		Interaction: &discordgo.Interaction{
-			Type: discordgo.InteractionApplicationCommand,
-			Data: discordgo.ApplicationCommandInteractionData{
-				Name: "test",
-			},
-		},
-	})
-
-	if !errors.Is(err, expectedError) {
-		t.Errorf("Expected error %v, got %v", expectedError, err)
-	}
-}
-
-func TestNew(t *testing.T) {
-	switchboard := New()
-
-	if switchboard.Commands == nil {
-		t.Error("Expected Commands to be initialized, got nil")
 	}
 }
